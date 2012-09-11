615056643b6f770421e3e5ed5269d67033bd591b
Tuomas Koponen <tuomas.koponen@oss-solutions.fi>
2012-09-11 20:19:57 +0300

    Here is start..
    Some planning, coding and implementing and thats it..

---
+++ publicHealthCare/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PublicHealthCare::Application.config.secret_token = 'ec1a947669bc491569fcc000861795e9c88c335b2022de3754c154b0ee48ba2e999b0b16ef04f4d99b9a23196908b8cb5bf361a92f2e3e68c03b269f8eaabb7b'
